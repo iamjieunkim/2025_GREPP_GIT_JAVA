ee1b3e1adebb196ab03ff32a4219bbd662e1a7e6
Jieun Kim <83564946+iamjieunkim@users.noreply.github.com>
2025-03-06 10:14:17 +0900

    3월 6일 수업내용

---
+++ src/main/java/lec2/operator/ConditionalOperator.java
@@ -0,0 +1,11 @@
+package lec2.operator;
+
+public class ConditionalOperator {
+
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        System.out.println((a>b)? a-b : a+b);
+    }
+}
